517c711d60649da5891768ddb210560c69d32d9a
Luminous <47873089+CristianQuiterio@users.noreply.github.com>
2023-04-14 16:45:10 -0400

    Create Recursion.java

---
+++ RecursionDemo/Recursion.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+    
+    public static int factorial(int n)
+    {
+        if(n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n-1); 
+        }
+    }
+}
